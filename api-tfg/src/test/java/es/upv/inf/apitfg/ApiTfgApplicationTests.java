57e1a854690243e2fddb37a4477770177c7e8901
Alejandro Martorell Hernandez <martorellhernandezalejandro@gmail.com>
2024-03-12 17:35:13 +0100

    first commit

---
+++ api-tfg/src/test/java/es/upv/inf/apitfg/ApiTfgApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upv.inf.apitfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
